cd02282ab97db37e92602d84f86f6fcca6a33bf2
Paul <paul.esperonarg@hotmail.com>
2024-02-06 21:14:58 -0300

    class navlinks change

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 
+// icon navbar 
+
 
 // scroll sections 
 
- 
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+ window.onscroll =() =>{
+
+  sections.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight; 
+    let id = sec.getAttribute('id')
+
+    if(top >= offset && top < offset + height){
+      // active navbar link 
+      navLinks.forEach(links =>{
+        links.classList.remove('activo')
+        document.querySelector('header nav a[href*= ' + id + ']').classList.add('activo')
+      })
+    }
+  })
+
+
+    // sticky header 
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+ }
